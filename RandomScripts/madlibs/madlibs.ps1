d68c25b47b0e3e8388dcf6fed605aabd5fe080b8
c-poteat <nellp7@yahoo.com>
2023-01-16 20:49:03 -0500

    Added some data validation

--- RandomScripts/madlibs/madlibs.ps1
+++ RandomScripts/madlibs/madlibs.ps1
@@ -2,7 +2,12 @@ function madlibs {
 
 $year = Read-Host "`nEnter a year in the past"
 $fullname = Read-Host "`nEnter your first and last name"
-$maleorfemale = "Are you a male or female?, Enter M or F"
+$maleorfemale = Read-Host "`nAre you a male or female?, Enter M or F"
+#Validating input
+while("M","F" -notcontains $maleorfemale)
+{
+ $maleorfemale = Read-Host "`nPlease enter your response (M or F)"
+}
 $heorshe = ""
 $jobtitle = Read-Host "`nEnter your job title"
 $randomchar = Read-Host "`nEnter a character's name"
@@ -22,14 +27,14 @@ Sleep 4
 
 #Modified the story slightly for madlibs
 
-$story = "`nDuring the year $year, a man named $fullname (also known as Neo), 
+$story = "`nDuring the year $year, $fullname (also known as Neo), 
 lives an ordinary life. A $jobtitle by day and a computer hacker 
 by night, $heorshe sits alone at home by a monitor, waiting for a sign, a 
 signal - from what or whom $heorshe doesn't know - until one night, a 
 mysterious person named $randomchar seeks $firststring out and introduces $firststring to that 
 faceless character $heorshe has been waiting for: $anotherchar. A messiah of sorts, 
 $anotherchar presents Neo with the truth about the world by shedding light on 
-the dark secrets that have troubled Neo for so long."`n
+the dark secrets that have troubled Neo for so long.`n"
 
 $story
 }
